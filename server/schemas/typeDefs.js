78fe60ed90b0b714a717522344805f8ecdcefcc7
Jason Wisniewski <jwizinpa95@gmail.com>
2022-03-14 13:06:19 -0600

    error message incorrect credentails

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,14 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
   type Thought {
     _id: ID
@@ -13,7 +21,6 @@ const typeDefs = gql`
     reactions: [Reaction]
   }
 
-
   type Reaction {
     _id: ID
     reactionBody: String
@@ -21,20 +28,16 @@ const typeDefs = gql`
     username: String
   }
 
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
-  }
-
   type Query {
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
   }
 `;
 // export the typeDefs
