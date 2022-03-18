a84a42d517bb5cb24d46ff4f7a92713569107c6f
Jason Wisniewski <jwizinpa95@gmail.com>
2022-03-18 09:25:01 -0600

    jwt tokens for logged in users

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,9 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -21,8 +21,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
